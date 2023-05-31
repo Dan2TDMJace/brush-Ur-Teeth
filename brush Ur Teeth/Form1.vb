9a0fd8e00925b5f87370fb6cd4a25eda45e7e255
Dan2TDMJace <112831108+Dan2TDMJace@users.noreply.github.com>
2023-05-30 19:05:04 -0500

    Release 1.0
    A "friendly reminder" to brush your teeth with Task Scheduler.
    
    Features:
    -Important notitication
    -25 tabs open that play an alarm sound when you do anything with the noticication

---
+++ brush Ur Teeth/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Do
+            MsgBox("brush Ur Teeth now", MsgBoxStyle.Exclamation, "Important Noticication!1!")
+            Process.Start("https://www.youtube.com/watch?v=rUkzZTGE6jI")
+            Process.Start("https://www.youtube.com/watch?v=rUkzZTGE6jI")
+            Process.Start("https://www.youtube.com/watch?v=rUkzZTGE6jI")
+            Process.Start("https://www.youtube.com/watch?v=rUkzZTGE6jI")
+            Process.Start("https://www.youtube.com/watch?v=rUkzZTGE6jI")
+            Process.Start("https://www.youtube.com/watch?v=rUkzZTGE6jI")
+            Process.Start("https://www.youtube.com/watch?v=rUkzZTGE6jI")
+            Process.Start("https://www.youtube.com/watch?v=rUkzZTGE6jI")
+            Process.Start("https://www.youtube.com/watch?v=rUkzZTGE6jI")
+            Process.Start("https://www.youtube.com/watch?v=rUkzZTGE6jI")
+            Process.Start("https://www.youtube.com/watch?v=rUkzZTGE6jI")
+            Process.Start("https://www.youtube.com/watch?v=rUkzZTGE6jI")
+            Process.Start("https://www.youtube.com/watch?v=rUkzZTGE6jI")
+            Process.Start("https://www.youtube.com/watch?v=rUkzZTGE6jI")
+            Process.Start("https://www.youtube.com/watch?v=rUkzZTGE6jI")
+            Process.Start("https://www.youtube.com/watch?v=rUkzZTGE6jI")
+            Process.Start("https://www.youtube.com/watch?v=rUkzZTGE6jI")
+            Process.Start("https://www.youtube.com/watch?v=rUkzZTGE6jI")
+            Process.Start("https://www.youtube.com/watch?v=rUkzZTGE6jI")
+            Process.Start("https://www.youtube.com/watch?v=rUkzZTGE6jI")
+        Loop
+    End Sub
+End Class
